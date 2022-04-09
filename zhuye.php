ecfb3f3d41001b80cfcd9a5584e05f2ffbeac30c
cjy114514 <97960867+cjy114514@users.noreply.github.com>
2022-04-09 09:42:53 +0800

    Create zhuye.php

---
+++ zhuye.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<style>
+body {background-color: powderblue;}
+</style>
+</head>
+
+<body>
+
+<h1 style="color:black;">首页<br><h1>
+<p style="color:red;">这是陈径优的网站<br><p>
+<p style="color:red;">目前支持：<br><p>
+<p style="color:green;">---论坛<br><p>
+<button onclick="document.location='luntan.php'">论坛</button>
+
+
+</body>
+
+</html>
